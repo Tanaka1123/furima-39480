de2aa2c24890b707bc930e08ea0aecb9ecb3b147
Tanaka1123 <tamatech11@gmail.com>
2023-06-22 16:48:12 +0900

    add deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,6 +2,9 @@
 # capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
 lock '3.17.3'
 
+# gitのデフォルトbranchをmainと認識するよう指定
+set :branch, 'main'
+
 # Capistranoのログの表示に利用する
 set :application, 'furima-39480'
 
